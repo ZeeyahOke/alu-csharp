f1ddf2ca0a56279fbd30d8f5a7124c7c95084724
ZeeyahOke <f.oke@alustudent.com>
2026-01-14 10:04:09 +0100

    task 0

---
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Script to initialize a new C# project
+
+# Create a new directory for the project
+mkdir -p 0-new_project
+
+# Navigate to the directory
+cd 0-new_project
+
+# Initialize a new C# console application
+dotnet new console
