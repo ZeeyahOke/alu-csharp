ebf77e1c92d6bf5a9a727a07720dfc7bdffad0e2
ZeeyahOke <f.oke@alustudent.com>
2026-01-17 14:15:13 +0100

    task 4

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 972; 
+        Console.WriteLine($"{number} Mission Street");
+    }
+}
